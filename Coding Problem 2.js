fad4eef03e1e8b110d93fbc5142f3e8891cf4312
santchaycabel <santchaycabel18@gmail.com>
2025-03-30 17:01:52 +0800

    Update Coding Problem 2.js

--- Coding Problem 2.js
+++ Coding Problem 2.js
@@ -7,10 +7,10 @@
 // Student Name
 // Use variables to store this information and log it to the console following the string “[Student Name] is currently enrolled in [Subject Title] with a class schedule of [Class Schedule] at room [Classroom]. The instructor for the subject is [Class Instructor]”
 
-let studentName = prompt("Please enter the student's name:");
-let subjectTitle = prompt("Please enter the subject title:");
-let classSchedule = prompt("Please enter the class schedule (Time, Days):");
-let classroom = prompt("Please enter the classroom:");
-let classInstructor = prompt("Please enter the class instructor's name:");
+let subjectTitle = prompt("Enter the Subject Title:");
+let classSchedule = prompt("Enter the Class Schedule (Time, Days):");
+let classroom = prompt("Enter the Classroom:");
+let classInstructor = prompt("Enter the Class Instructor:");
+let studentName = prompt("Enter the Student Name:");
 
-console.log(${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor});
+console.log(studentName + " is currently enrolled in " + subjectTitle + " with a class schedule of " + classSchedule + " at room " + classroom + ". The instructor for the subject is " + classInstructor + ".");
