1af5fe135fd8e3d573b5b69df6e0e9f8902900bc
santchaycabel <santchaycabel18@gmail.com>
2025-03-30 19:50:13 +0800

    Update Coding Problem 7-8.js

--- Coding Problem 7-8.js
+++ Coding Problem 7-8.js
@@ -4,32 +4,38 @@ let WebDev = [];
 let subject = prompt("Select a subject: (A) DSA or (B) WebDev");
 
 while (true) {
-    let choice = prompt("1. Enroll\n2. Unenroll\n3. Change Subject\n4. Exit");
+let choice = prompt("A. Enroll\nB. Unenroll\nC. Change Subject\nD. Exit");
 
-    if (choice === "1") {
-        let student = prompt("Enter student name to enroll:");
-        subject === "A" ? DSA.push(student) : WebDev.push(student);
-        alert(${student} enrolled successfully.);
-        
-    } else if (choice === "2") {
-        let student = prompt("Enter student name to unenroll:");
-        if (subject === "A") {
-            DSA = DSA.filter(name => name !== student);
-        } else {
-            WebDev = WebDev.filter(name => name !== student);
-        }
-        alert(${student} unenrolled successfully.);
-        
-    } else if (choice === "3") {
-        subject = prompt("Select a subject: (A) DSA or (B) WebDev").toUpperCase();
-        
-    } else if (choice === "4") {
-        alert(DSA Students: ${DSA.join(", ") || "None"}\nWebDev Students: ${WebDev.join(", ") || "None"});
-        alert("Program terminated.");
-        break;
-        
-    } else {
-        alert("Invalid choice. Try again.");
-    }
+if (choice === "A") {  
+    let student = prompt("Enter student name to enroll:");  
+    if (subject === "A") {  
+        DSA.push(student);  
+    } else {  
+        WebDev.push(student);  
+    }  
+    alert(student + " enrolled successfully.");  
+
+} else if (choice === "B") {  
+    let student = prompt("Enter student name to unenroll:");  
+    if (subject === "A") {  
+        DSA = DSA.splice();  
+    } else {  
+        WebDev = WebDev.splice();  
+    }  
+    alert(student + "unenrolled successfully.");  
+
+} else if (choice === "C") {  
+    subjects = prompt("Select a subject: (A) DSA or (B) WebDev");  
+
+} else if (choice === "D") {  
+    alert("DSA Students: " + DSA + "\nWebDev Students: " + WebDev);  
+    alert("Program terminated.");  
+    break;  
+
+} else {  
+    alert("Invalid choice. Try again.");  
 }
+
+}
+
 
