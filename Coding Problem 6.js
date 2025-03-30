5baaae2ef06e625640264c7ad916826ea69f4071
santchaycabel <santchaycabel18@gmail.com>
2025-03-30 19:46:00 +0800

    Update Coding Problem 6.js

--- Coding Problem 6.js
+++ Coding Problem 6.js
@@ -1,20 +1,18 @@
-let createGroceryList = prompt("Would you like to create a grocery list? (Y/N)");
+let groceryList = [];
 
-if (createGroceryList.toUpperCase() === "Y") {
-    let numberOfItems = parseInt(prompt("How many items would you like to add to your grocery list?"));
+let createList = prompt("Would you like to create a grocery list? (Y/N)");
 
-    let groceryList = [];
+if (createList) { 
+    let numberOfItems = Number(prompt("How many items would you like to add to your grocery list?"));
 
     for (let i = 0; i < numberOfItems; i++) {
-        let item = prompt(Item ${i + 1}:);
+        let item = prompt("Enter item " + (i + 1) + ":");
         groceryList.push(item);
     }
 
     groceryList.sort();
-
-    alert(Your sorted grocery list:\n- ${groceryList.join("\n- ")});
+    alert("Your sorted grocery list: " + groceryList);
     console.log("Your sorted grocery list:", groceryList);
 } else {
     alert("No grocery list created.");
 }
-
